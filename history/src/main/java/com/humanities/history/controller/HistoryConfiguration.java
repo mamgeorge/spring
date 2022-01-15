195fae9395fce302230cf0b029a4d27d2854856e
mamgeorge <mamgeorge@yahoo.com>
2022-01-15 11:54:50 -0500

    added actuators, rdbms

--- history/src/main/java/com/humanities/history/controller/HistoryConfiguration.java
+++ history/src/main/java/com/humanities/history/controller/HistoryConfiguration.java
@@ -1,6 +1,7 @@
 package com.humanities.history.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.thymeleaf.spring5.SpringTemplateEngine;
 import org.thymeleaf.spring5.view.ThymeleafViewResolver;
 import org.thymeleaf.templateresolver.ServletContextTemplateResolver;
