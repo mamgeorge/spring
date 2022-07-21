24fd3685f74b3be9bec7450d2840691348686abc
mamgeorge <mamgeorge@yahoo.com>
2022-07-21 17:42:49 -0400

    removed history app

--- history/src/main/java/com/humanities/history/configuration/HistoryRepository.java
+++
@@ -1,6 +0,0 @@
-package com.humanities.history.configuration;
-
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository public interface HistoryRepository extends CrudRepository<History, Long> { }
--- history/src/test/java/com/humanities/history/HistoryApplicationTests.java
+++
@@ -1,9 +0,0 @@
-package com.humanities.history;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest class HistoryApplicationTests {
-
-	@Test void contextLoads( ) { }
-}
