d8ba08fe566db30de84245b47250d7618632bd75
mamgeorge <mamgeorge@yahoo.com>
2023-02-10 21:25:05 -0500

    temp

---
+++ sb_sec_oauth/src/main/java/com/example/sec_oauth/SecOauthApplication.java
@@ -0,0 +1,14 @@
+package com.example.sec_oauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecOauthApplication {
+
+	// https://spring.io/guides/tutorials/spring-boot-oauth2/
+	public static void main(String[] args) {
+		SpringApplication.run(SecOauthApplication.class, args);
+	}
+
+}
---
+++ sb_sec_oauth/src/test/java/com/example/sec_oauth/SecOauthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sec_oauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecOauthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
