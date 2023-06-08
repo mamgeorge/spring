69f67abd91dc2f45ba8cc50a7f7a31b208c0fb51
mamgeorge <mamgeorge@yahoo.com>
2023-06-08 16:36:07 -0400

    catch NSEE error

--- sb_testing/src/test/java/com/basics/testmore/util/SeleniumTests.java
+++ sb_testing/src/test/java/com/basics/testmore/util/SeleniumTests.java
@@ -4,6 +4,7 @@ import org.apache.commons.lang3.RandomStringUtils;
 import org.apache.commons.lang3.StringUtils;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.Platform;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -35,12 +36,13 @@ public class SeleniumTests {
 	/*
 		MicrosoftEdge Version: 114.0.1823.41 (Official build) (64-bit)
 		Google Chrome Version: 114.0.5735.110 (Official Build) (64-bit)
-		USERNAME: mamge, OS: Windows_NT, os.arch: amd64, os.name: Windows 10, os.version: 10.0
 		RemoteWebDriver only possible if Selenium server exists
-		pom.xml: org.seleniumhq.selenium:selenium-java:4.9.0
-			org.seleniumhq.selenium:selenium-http-jdk-client:4.5.0 ?
-		for hypervisor systems, copy the primary application folders into hard storage
-		see: FirefoxOptions, FirefoxDriver
+
+		pom.xml
+			org.seleniumhq.selenium:selenium-api,reomote-driver:4.10.0
+			?org.seleniumhq.selenium:selenium-http-jdk-client:4.5.0
+
+		USERNAME: mamge, OS: Windows_NT, os.arch: amd64, os.name: Windows 10, os.version: 10.0
 
 		org.openqa.selenium.SessionNotCreatedException: Could not start a new session.
 			Possible causes are invalid address of the remote server or browser start-up failure.
@@ -63,10 +65,10 @@ public class SeleniumTests {
 			options.addArguments("--incognito");
 			options.addArguments("--remote-allow-origins=*");
 			options.addArguments("--disable-extensions");
+			options.addArguments("--disable-dev-shm-usage");
 
 			options.addArguments("--disable-in-process-stack-trace");
 			options.addArguments("--disable-logging");
-			options.addArguments("--disable-dev-shm-usage");
 			options.addArguments("--log-level=3");
 			options.addArguments("--output=/dev/null");
 			options.addArguments("--silent");
@@ -81,7 +83,7 @@ public class SeleniumTests {
 		}
 
 		WebDriver webDriver = new ChromeDriver(options);
-		webDriver.get(URL_WEBPAGES[1]);
+		webDriver.get(URL_WEBPAGES[0]);
 		webDriver.manage().timeouts().implicitlyWait(Duration.ofMillis(1500));
 
 		txtLines += "getCurrentUrl: " + webDriver.getCurrentUrl() + EOL;
@@ -90,24 +92,26 @@ public class SeleniumTests {
 		// txtLines += "getPageSource: " + webDriver.getPageSource() + EOL;
 
 		if(IS_SUBMIT) {
+			try {
+				WebElement txtBoxUser = webDriver.findElement(By.name("my-text"));
+				WebElement txtBoxPass = webDriver.findElement(By.name("my-password"));
+				WebElement submitButton = webDriver.findElement(By.className("btn-outline-primary"));
 
-			WebElement txtBoxUser = webDriver.findElement(By.name("my-text"));
-			WebElement txtBoxPass = webDriver.findElement(By.name("my-password"));
-			WebElement submitButton = webDriver.findElement(By.className("btn-outline-primary"));
+				String password = RandomStringUtils.randomAlphabetic(8);
+				txtLines += "########: " + password + EOL;
+				txtBoxUser.clear();
+				txtBoxPass.clear();
+				txtBoxUser.sendKeys(System.getenv("USERNAME"));
+				txtBoxPass.sendKeys(password);
+				submitButton.click();
 
-			String password = RandomStringUtils.randomAlphabetic(8);
-			txtLines += "########: " + password + EOL;
-			txtBoxUser.clear();
-			txtBoxPass.clear();
-			txtBoxUser.sendKeys(System.getenv("USERNAME"));
-			txtBoxPass.sendKeys(password);
-			submitButton.click();
-
-			txtLines += "getTitle: " + webDriver.getTitle() + EOL;
+				txtLines += "getTitle: " + webDriver.getTitle() + EOL;
+			}
+			catch (NoSuchElementException ex) { System.out.println("NSEE ERROR: " + ex.getMessage()); }
 		}
 
-		try { TimeUnit.SECONDS.sleep(5); }
-		catch (InterruptedException ex) { System.out.println("ERROR: " + ex.getMessage()); }
+		try { TimeUnit.SECONDS.sleep(2); }
+		catch (InterruptedException ex) { System.out.println("IE ERROR: " + ex.getMessage()); }
 
 		webDriver.quit();
 		System.out.println(txtLines);
