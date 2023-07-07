9063d730b6aa0b0353e28934e3e23d459f719b9b
mamgeorge <mamgeorge@yahoo.com>
2023-07-07 11:31:11 -0400

    added coverage

---
+++ sb_testing/src/test/java/com/basics/testmore/model/CityTest.java
@@ -0,0 +1,26 @@
+package com.basics.testmore.model;
+
+import org.junit.jupiter.api.Test;
+
+import static com.basics.testmore.util.UtilityMain.exposeObject;
+import static org.junit.jupiter.api.Assertions.*;
+
+class CityTest {
+
+	@Test
+	void testEquals( ) {
+
+		boolean isCity = false;
+		City city = new City();
+
+		isCity = city.equals(new City());
+		System.out.println("isCity: " + isCity);
+
+		city.setName("Columbus");
+		isCity = city.equals(new City());
+		System.out.println("isCity: " + isCity);
+
+		System.out.println(exposeObject(city));
+		assertNotNull(city);
+	}
+}
