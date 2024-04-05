ed960466d437f2b4c90d1c4cfc0a8666cb549452
mamgeorge <mamgeorge@yahoo.com>
2024-04-04 23:51:22 -0400

    initial commit

---
+++ applications/graphqlo/src/main/java/com/example/graphqlo/GraphqloApp.java
@@ -0,0 +1,13 @@
+package com.example.graphqlo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GraphqloApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GraphqloApp.class, args);
+	}
+
+}
---
+++ applications/graphqlo/src/test/java/com/example/graphqlo/GraphqloAppTests.java
@@ -0,0 +1,13 @@
+package com.example.graphqlo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+// @SpringBootTest
+class GraphqloAppTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
