197f9dcbcdb19cb48981ae67660f5b730c6088ac
mamgeorge <mamgeorge@yahoo.com>
2024-04-17 13:07:27 -0400

    add new project folder name

---
+++ applications/graphqld/src/main/java/com/example/graphqld/GraphqldApp.java
@@ -0,0 +1,13 @@
+package com.example.graphqld;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GraphqldApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GraphqldApp.class, args);
+	}
+
+}
---
+++ applications/graphqld/src/main/java/com/example/graphqld/persistence/CityRepository.java
@@ -0,0 +1,13 @@
+package com.example.graphqld.persistence;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface CityRepository extends JpaRepository<City, Integer> {
+
+	@Override List<City> findAll( );
+}
+
