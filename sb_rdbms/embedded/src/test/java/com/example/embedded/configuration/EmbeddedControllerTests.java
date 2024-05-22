2874ad5f2d50e94c89a423f3ee59925a8afd053b
mamgeorge <mamgeorge@yahoo.com>
2024-05-22 15:09:22 -0400

    simplified tests

--- sb_rdbms/embedded/src/test/java/com/example/embedded/configuration/EmbeddedControllerTests.java
+++ sb_rdbms/embedded/src/test/java/com/example/embedded/configuration/EmbeddedControllerTests.java
@@ -1,9 +1,7 @@
 package com.example.embedded.configuration;
 
-import java.io.StringWriter;
-import java.util.logging.Logger;
-
 import com.example.embedded.model.City;
+import com.example.embedded.model.CityRepository;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
@@ -11,17 +9,12 @@ import org.junit.jupiter.api.TestInstance.Lifecycle;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.web.client.TestRestTemplate;
-import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.ResultActions;
-import org.springframework.util.Assert;
+import org.springframework.context.ApplicationContext;
 import org.springframework.web.servlet.ModelAndView;
 
-import static org.hamcrest.Matchers.containsString;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import java.util.logging.Logger;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 // https://spring.io/guides/gs/testing-web/
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT) @AutoConfigureMockMvc
@@ -30,86 +23,47 @@ public class EmbeddedControllerTests {
 
 	public static final Logger LOGGER = Logger.getLogger(EmbeddedControllerTests.class.getName());
 
+	@Autowired private CityRepository cityRepository;
+	@Autowired private ApplicationContext applicationContext;
 	@Autowired private EmbeddedController sbController = null;
-	@Autowired private TestRestTemplate restTemplate;
-	@Autowired private MockMvc mockMvc;
 
-	private static final String ASSERTION = "ASSERTION";
 	private static final String EOL = "\n";
-	private static final String TAB = "\t";
-	private static final String LOCALHOST = "http://localhost:";
-	private static final int PORT = 3000;
 
 	@BeforeAll void setup() {
-		//
-		//sbController = new SbController();
-		//restTemplate = new TestRestTemplate();
+
+		sbController = new EmbeddedController(cityRepository, applicationContext);
 	}
 
 	@Test void test_contextLoads() {
-		//
-		System.out.println(EOL + "test_contextLoads()");
-		LOGGER.info(TAB + "sbController: " + sbController.toString());
-		Assert.isTrue(sbController != null, ASSERTION);
-	}
 
-	@Test void test_root_RT() {
-		//
-		System.out.println(EOL + "test_root_RT()");
-		String txtLines = "";
-		String html = this.restTemplate.getForObject(LOCALHOST + PORT + "/", String.class);
-		txtLines += TAB + "PORT: " + PORT + EOL;
-		txtLines += TAB + "html: " + html;
-		System.out.println(txtLines);
-		Assert.isTrue(html.contains("home"), ASSERTION);
-	}
-
-	@Test void test_root_MM() throws Exception {
-		//
-		System.out.println(EOL + "test_root_MM()");
-		StringWriter stringWriter = new StringWriter();
-		stringWriter.append("####");
-		// OutputStream outputStream = System.out;
-		//
-		ResultActions resultActions = this.mockMvc.perform(get("/")).andDo(print(stringWriter))
-			.andExpect(status().isOk()).andExpect(content().string(containsString("home")));
-		System.out.println(TAB + "RAS: " + resultActions + EOL);
-		// System.out.println(TAB + "RAO: " + resultActions.andDo(print(outputStream)));
+		LOGGER.info("sbController: " + sbController.toString());
+		assertNotNull(sbController);
 	}
 
 	@Test void test_home() {
-		//
-		System.out.println(EOL + "test_home()");
+
 		ModelAndView MAV = sbController.home();
-		System.out.println(TAB + "sbController.root(): " + MAV.getViewName());
-		Assert.isTrue(MAV.getViewName().equals("index"), ASSERTION);
+		System.out.println("sbController.root(): " + MAV.getViewName());
+		assertNotNull(MAV);
 	}
 
 	@Test void test_showCities() {
-		//
-		System.out.println(EOL + "test_showCities()");
-		String txtLines = "";
+
 		String[] cities = sbController.showCities().split(EOL);
-		txtLines += cities[0] + " / " + "len: " + cities.length;
-		System.out.println("txtLines: " + txtLines);
-		Assert.isTrue(cities.length > 20, ASSERTION);
+		System.out.println("cities: " + cities.length);
+		assertNotNull(cities);
 	}
 
 	@Test void test_showCityRnd() {
-		//
-		System.out.println(EOL + "test_showCity()");
+
 		City city = sbController.showCityRnd();
-		String txtLines = city.toString();
-		System.out.println("txtLines: " + txtLines);
-		System.out.println("city.getName(): " + city.getName());
-		Assert.isTrue(city.getName().equals("CN Chongqing"), ASSERTION);
+		assertNotNull(city);
 	}
 
-	@Test void test_showCity_val() {
-		//
-		System.out.println(EOL + "test_showCity_val()");
-		String html = this.restTemplate.getForObject(LOCALHOST + PORT + "/showCity/5", String.class);
-		System.out.println("html: " + html.toString());
-		Assert.isTrue(html.contains("MexicoCity"), ASSERTION);
+	@Test void test_showCity( ) {
+
+		ModelAndView MAV = sbController.showCity("5");
+		City city = (City) MAV.getModel().get("city");
+		assertNotNull(city);
 	}
 }
