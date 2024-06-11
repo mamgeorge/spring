037758ba0161aea63a229abafd3f0e6423b02e4c
mamgeorge <mamgeorge@yahoo.com>
2024-06-10 22:56:12 -0400

    added controllers

--- sb_ntier/src/main/java/com/example/ntier/configs/NtierController.java
+++ sb_ntier/src/main/java/com/example/ntier/configs/NtierController.java
@@ -1,14 +1,66 @@
 package com.example.ntier.configs;
 
+import com.example.ntier.persistence.User;
+import com.example.ntier.persistence.UserService;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.time.Instant;
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
-@RestController
+import static org.springframework.http.HttpStatus.NOT_FOUND;
+
+@RestController @RequestMapping( path = "/api")
 public class NtierController {
+
+	private UserService userService;
+
+	@Autowired public NtierController(UserService userService) {
+		this.userService=userService;
+	}
 
 	@GetMapping( { "/" } ) String root( ) { return Instant.now().toString(); }
 
 	@GetMapping( "/getMessage" ) Message getMessage( ) { return new Message(Instant.now().toString()); }
+
+	@GetMapping( "/getAllUsers" )
+	public List<User> getAllUsers() { return userService.getAllUsers(); }
+
+	@GetMapping( "/getUser/{userUid}" )
+	public ResponseEntity<?> getUser(@PathVariable("userUid") UUID userUid) {
+
+		/*
+			Optional<User> optional = userService.getUser(userUid);
+			if(optional.isPresent())
+			{ return ResponseEntity.ok(optional.get()); } else
+			{ return ResponseEntity.status(NOT_FOUND).body(new ErrorMessages("userUid: " + userUid + " not found!")); }
+		*/
+		return userService.getUser(userUid)
+			.<ResponseEntity<?>>map(ResponseEntity::ok)
+			.orElseGet(()-> ResponseEntity.status(NOT_FOUND)
+				.body(new ErrorMessages("userUid: " + userUid + " not found!")));
+	}
+
+	@PostMapping( "/insertUser" )
+	public ResponseEntity<Integer> insertUser(User user) {
+
+		int intResult = userService.insertUser(user);
+		if (intResult==1) { return ResponseEntity.ok().build(); }
+		return ResponseEntity.badRequest().build();
+	}
+
+	// utility
+	@Getter @Setter @AllArgsConstructor @ToString
+	class ErrorMessages{ private String errorMessage; }
 }
--- sb_ntier/src/test/java/com/example/ntier/configs/NtierControllerTest.java
+++ sb_ntier/src/test/java/com/example/ntier/configs/NtierControllerTest.java
@@ -1,20 +1,36 @@
 package com.example.ntier.configs;
 
+import com.example.ntier.persistence.User;
+import com.example.ntier.persistence.UserDaoData;
+import com.example.ntier.persistence.UserService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.http.ResponseEntity;
 
 import java.time.Instant;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
+@ExtendWith( { MockitoExtension.class } )
 class NtierControllerTest {
 
 	private Message message;
 	private NtierController ntierController;
+	@Mock private UserService userService;
 
 	@BeforeEach void setUp( ) {
 		message = new Message(Instant.now().toString());
-		ntierController = new NtierController();
+		ntierController = new NtierController(userService);
 	}
 
 	@Test void root( ) {
@@ -26,8 +42,24 @@ class NtierControllerTest {
 
 	@Test void getMessage( ) {
 
-		Message message = ntierController.getMessage();
+		message = ntierController.getMessage();
 		System.out.println(message);
 		assertThat(message).isNotNull();
+	}
+
+	@Test void getAllUsers( ) {
+
+		when(ntierController.getAllUsers()).thenReturn(new ArrayList());
+		List<User> list = ntierController.getAllUsers();
+		System.out.println(list);
+		assertThat(list).isNotNull();
+	}
+
+	@Test void getUser( ) {
+
+		ResponseEntity<?> response;
+		response = ntierController.getUser(UUID.randomUUID());
+		System.out.println(response.getBody());
+		assertThat(response).isNotNull();
 	}
 }
