52a4358c0b6dafc46b8fc4e1fafff3fc073a43f7
mamgeorge <mamgeorge@yahoo.com>
2026-06-10 18:10:06 -0400

    added post run code

--- sb_rdbms/sqlite/src/main/java/com/basics/dbsqlite/DbSqliteApp.java
+++ sb_rdbms/sqlite/src/main/java/com/basics/dbsqlite/DbSqliteApp.java
@@ -2,14 +2,22 @@ package com.basics.dbsqlite;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.EventListener;
+import org.springframework.stereotype.Component;
 
 @SpringBootApplication
 public class DbSqliteApp {
 
 	public static void main(String[] strings) throws Throwable {
 
-		System.out.println("#### SecuringWebApp ####");
+		System.out.println("#### DbSqliteApp started! ####");
 		SpringApplication.run(DbSqliteApp.class, strings);
-		System.out.println("RUNNING!!!");
 	}
+}
+@Component class AppStartupRunner {
+
+    @EventListener(ApplicationReadyEvent.class)
+    public void runAfterStartup() {
+		System.out.println("#### DbSqliteApp running! ####");    }
 }
